50f698daecd1a308d2b5cfd6634cd8c64c31823b
IKAROS <1027alex@gmail.com>
2025-04-16 21:15:26 +0900

    2025_04_16_21:14

---
+++ LeetCode/9_01.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reverse = str(x)[::-1]
+
+        if str(x) == reverse:
+            return True
+        else:
+            return False
